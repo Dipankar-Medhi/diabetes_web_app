7e984b248d3b2709c2722fb53a45835caa76b713
Dipankar-Medhi <dipumedhi99@gmail.com>
2021-07-09 16:04:03 +0530

    fixed api

---
+++ components/hero.js
@@ -0,0 +1,15 @@
+function Hero() {
+    return (
+        <div>
+            <main className="w-screen py-60 grid place-content-center">
+                <div className="text-center">
+
+                    <h1 className="font-sans font-bold text-5xl py-6">PIMA</h1>
+                    <p className="font-serif font-medium">Diabetes Prediction <br></br> Machine Learning end to end project</p>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Hero
